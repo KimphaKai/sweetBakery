bd821a6741e2d51941fe148ac960b7724a2e811e
cynthia0713 <chsieh0713@gmail.com>
2022-02-16 14:30:56 +0800

    added login/signup/forgetPW modal

--- 前端/javascript/script.js
+++ 前端/javascript/script.js
@@ -17,11 +17,11 @@ $(document).ready(function () {
   })
 
   $(".fa-shopping-cart").on("click", () => {
-    $(".modal-overlay").show()
+    $(".modal-container").show()
   })
 
   $(".closeModal").on("click", function () {
-    $(".modal-overlay").hide()
+    $(".modal-container").hide()
   })
 
   $(".burgerMenu").on("click", function (e) {
@@ -34,59 +34,39 @@ $(document).ready(function () {
     }
   })
 
+  //Sign in and sign up event
+  $("#signUp").on("click", () => {
+    $(".container").addClass("right-panel-active")
+    console.log("signup")
+  })
+
+  $("#signIn").on("click", () => {
+    $(".container").removeClass("right-panel-active")
+    console.log("signin")
+  })
+
+  $(".userIcon").on("click", function () {
+    $(".loginModal").show()
+  })
+
+  //Close Modal event
+  $(".closeModal").on("click", function () {
+    $(".loginModal").hide()
+    $(".forgetPwModal").hide()
+  })
+
+
+  $(".forgetPwBtn").on("click", function () {
+    $(".loginModal").hide()
+    $(".forgetPwModal").show()
+  })
+
+  $(".loginBtn").on("click", function () {
+    $(".forgetPwModal").hide()
+    $(".loginModal").show()
+  })
 
 });
-
-
-//  // Select required elements from the DOM
-//  const modal = document.querySelector("#modal");
-//  const body = document.querySelector("body");
-
-//  const showModal = function (e) {
-//      modal.classList.toggle("hidden");
-
-//      if (!modal.classList.contains("hidden")) {
-//          // Disable scroll
-//          body.style.overflow = "hidden";
-//      } else {
-//          // Enable scroll
-//          body.style.overflow = "auto";
-//      }
-//  };
-
-
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal)
-//   })
-// })
-
-// overlay.addEventListener('click', () => {
-//   const modals = document.querySelectorAll('.modal.active')
-//   modals.forEach(modal => {
-//     closeModal(modal)
-//   })
-// })
-
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal')
-//     closeModal(modal)
-//   })
-// })
-
-// function openModal(modal) {
-//   if (modal == null) return
-//   modal.classList.add('active')
-//   overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//   if (modal == null) return
-//   modal.classList.remove('active')
-//   overlay.classList.remove('active')
-// }
 
 
 let thumbnails = document.getElementsByClassName('thumbnail')
