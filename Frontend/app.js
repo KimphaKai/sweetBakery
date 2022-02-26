244ca34da23fbf44957f087f7a64214146607cfd
cynthia0713 <chsieh0713@gmail.com>
2022-02-27 01:26:05 +0800

    added router to each page

--- Frontend/app.js
+++ Frontend/app.js
@@ -1,63 +1,39 @@
-const express = require("express");
-const app = express();
+var express = require("express");
+var app = express();
+var conn = require('./db');
+var homeRouter = require('./router/home');
+var checkoutRouter = require('./router/checkout');
+var cartRouter = require('./router/cart');
+var classDetailRouter = require('./router/classDetail');
+var classesRouter = require('./router/classes');
+var classFormRouter = require('./router/classForm');
+var passwordUpdateRouter = require('./router/passwordUpdate');
+var productDetailRouter = require('./router/productDetail');
+var productsRouter = require('./router/products');
+var registeredRouter = require('./router/registered');
+const userRouter = require("./router/user");
+const orderHistoryRouter = require("./router/orderHistory");
+const ordersRouter = require("./router/orders");
 app.listen(3000);
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+app.use('/', homeRouter);
+app.use('/checkout', checkoutRouter);
+app.use('/cart', cartRouter);
+app.use('/classDetail', classDetailRouter);
+app.use('/classes', classesRouter);
+app.use('/classForm', classFormRouter);
+app.use('/passwordUpdate', passwordUpdateRouter);
+app.use('/productDetail', productDetailRouter);
+app.use('/products', productsRouter);
+app.use('/registered', registeredRouter);
+app.use('/user', userRouter);
+app.use('/orderHistory', orderHistoryRouter);
+app.use('/orders', ordersRouter);
 
-app.get("/home", function (req, res) {
-  res.render("index.ejs")
-})
 
 app.get("/about", function (req, res) {
-  res.render("about.ejs")
-})
-
-app.get("/checkout", function (req, res) {
-  res.render("checkout.ejs")
-})
-
-app.get("/cart", function (req, res) {
-  res.render("cart.ejs")
-})
-
-app.get("/classDetail", function (req, res) {
-  res.render("classDetail.ejs")
-})
-
-app.get("/classes", function (req, res) {
-  res.render("classes.ejs")
-})
-
-app.get("/classForm", function (req, res) {
-  res.render("classForm.ejs")
-})
-
-app.get("/passwordUpdate", function (req, res) {
-  res.render("passwordUpdate.ejs")
-})
-
-app.get("/productDetail", function (req, res) {
-  res.render("productDetail.ejs")
-})
-
-app.get("/products", function (req, res) {
-  res.render("products.ejs")
-})
-
-app.get("/registered", function (req, res) {
-  res.render("registered.ejs")
-})
-
-app.get("/user", function (req, res) {
-  res.render("user.ejs")
-})
-
-app.get("/orderHistory", function (req, res) {
-  res.render("orderHistory.ejs")
-})
-
-app.get("/orders", function (req, res) {
-  res.render("orders.ejs")
+  res.render("about")
 })
