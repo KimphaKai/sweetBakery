732ea519aba96d3d10646f9f92f553ea227f4b9b
HueiHu <chunghuei.tsao@gmail.com>
2022-03-09 01:21:16 +0800

    hueidone

--- Frontend/router/checkout.js
+++ Frontend/router/checkout.js
@@ -5,8 +5,9 @@ var db = require("../db");
 checkoutRouter.get('/', function (req, res) {
   let productInformation = {};
   let productPic = {};
+  req.session.username = 'dede';   //測試用
 // req.session.username
-  db.queryAsync('SELECT c.*, p.*,s.sizeName,(p.productPrice*c.productNum) AS subTotal FROM cartlist c JOIN product p ON(c.productId=p.productId) JOIN productsize s ON(p.sizeId=s.sizeId) WHERE c.memberId="dede"')
+  db.queryAsync(`SELECT c.*, p.*,s.sizeName,(p.productPrice*c.productNum) AS subTotal FROM cartlist c JOIN product p ON(c.productId=p.productId) JOIN productsize s ON(p.sizeId=s.sizeId) WHERE c.memberId="${req.session.username}"`)
     .then(category =>{
       productInformation = category;
       // console.log(category);
